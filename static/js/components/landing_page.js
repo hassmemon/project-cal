ec4aed372ca4281fae279c0aceb958cdde96164e
Hassaan <hassaanmmemon@gmail.com>
2022-03-12 12:02:42 +1100

    splash

--- static/js/components/landing_page.js
+++ static/js/components/landing_page.js
@@ -1,9 +1,14 @@
-function renderLandingPage(){
-    console.log('I\'m here!');
+function renderLandingPage() {
+    console.log("I'm here!");
     const page = document.querySelector('#page');
     page.innerHTML = `
-    <div>
-        <h1>Never forget about a task again!</h1>
+  <div>
+    <video class="background-video" controls autoplay loop>
+        <source src="https://res.cloudinary.com/dvbb6mvgw/video/upload/v1647045885/Slowmotion_River_Water_rtfqle.mp4" type="video/mp4">
+    </video>
+    <div class="video-overlay">
+        <h2>Welcome text</h2>
     </div>
+</div>
     `;
 }
