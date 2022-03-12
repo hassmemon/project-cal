6d31b7de45a3261232b9d3f6c2675f77545830ff
Franz <git@github.com:franz-net/project-cal.git>
2022-03-11 22:16:37 -0600

    added missing changes

--- static/js/components/landing_page.js
+++ static/js/components/landing_page.js
@@ -1,15 +1,17 @@
 function renderLandingPage() {
-    console.log("I'm here!")
-    const page = document.querySelector('#page')
+    console.log("I'm here!");
+    const page = document.querySelector('#page');
     page.innerHTML = `
   <div class="video-container">
     <video class="background-video" muted autoplay loop>
         <source src="https://res.cloudinary.com/dvbb6mvgw/video/upload/v1647045885/Slowmotion_River_Water_rtfqle.mp4" type="video/mp4">
     </video>
     <div class="caption">
-        <h2>Welcome text</h2>
+        <h1>Seamless Task Management</h1>
+        <h2 class="subcaption">Achieve Mastery<h2>
+        <h2 class="subcaption">Attain Flow<h2>
     </div>
     
 </div>
-    `
+    `;
 }
