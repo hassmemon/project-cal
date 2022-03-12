790381e28c42257592bfc3eb6dc079481624e437
Franz <git@github.com:franz-net/project-cal.git>
2022-03-11 19:59:26 -0600

    working with overlay

--- static/js/components/landing_page.js
+++ static/js/components/landing_page.js
@@ -3,7 +3,7 @@ function renderLandingPage() {
     const page = document.querySelector('#page')
     page.innerHTML = `
   <div class="video-container">
-    <video class="background-video" controls autoplay loop>
+    <video class="background-video" muted autoplay loop>
         <source src="https://res.cloudinary.com/dvbb6mvgw/video/upload/v1647045885/Slowmotion_River_Water_rtfqle.mp4" type="video/mp4">
     </video>
     <div class="caption">
